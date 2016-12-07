32b883fe33532c6b2e5b7b5625782efda79d7692
naman1255 <naman_jiandani@student.uml.edu>
2016-12-07 16:07:09 -0500

    Update firstScript.js

--- hw6/firstScript.js
+++ hw6/firstScript.js
@@ -7,15 +7,21 @@ function calculate() {
 	
     //getting input from user for row/col start and end values
     var first_r = Number(document.getElementById('firstRow').value);
-    var last_r = Number(document.getElementById('lastRow').value);
-    var first_c = Number(document.getElementById('firstCol').value);
-    var last_c = Number(document.getElementById('lastCol').value);
+    var last_r = document.getElementById('lastRow').value;
+    var first_c = document.getElementById('firstCol').value;
+    var last_c = document.getElementById('lastCol').value;
 	
     //alert for same input values for row/col
     if(first_r === last_r || first_c === last_c) {
         alert("Please enter unique values for start and end row/col indices");
         return;
     }
+	
+	
+	if ( (parseInt(first_r) === NaN) || (parseInt(first_c) === NaN) ||
+	    (parseInt(last_r) === NaN) || (parseInt(last_c) === NaN) ) {
+		return false;
+	}
 	
      //handles case when user enters greater number for row 1, lesser for row 2 (swaps them)
     if(last_r < first_r) {
@@ -59,27 +65,14 @@ function calculate() {
 }
 function create_table(new_array) {
 
+	
     //getting input from user for row/col start and end values
-    var first_r =Number(document.getElementById('firstRow').value) ;
+    var first_r = Number(document.getElementById('firstRow').value) ;
     var last_r = Number(document.getElementById('lastRow').value);
     var first_c = Number(document.getElementById('firstCol').value);
     var last_c = Number(document.getElementById('lastCol').value);
     
-    //alert for same input values for row/col
-     if(last_c < first_c) {
-        var temp3 = first_c;
-        first_c = last_c;
-        last_c = temp3;
-    }
-    
-    //handles case when user enters greater number for row 1, lesser for row 2 (swaps them)
-    if(last_r < first_r) {
-        var temp1 = first_r;
-        first_r = last_r;
-        last_r = temp1;
-    }
-
-
+ 
     //creating a table 
     //adding a top-left empty element
     //loop through entries for first row and fill into top row of form 
